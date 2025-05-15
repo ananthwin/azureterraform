63aedb86bf3d58b0998a14925261c116955a2fa6
arana <arananth@hotmail.com>
2025-05-15 17:47:46 -0400

    added variab

--- variables.tf
+++ variables.tf
@@ -13,16 +13,33 @@ variable "dotnet_version" {
 variable "client_id" {
   description = "Azure Service Principal Client ID"
   type        = string
-  default = "a0f98fdd-d80b-40e0-87e4-7201dd689145"
+  sensitive   = true
 }
 
 variable "client_secret" {
   description = "Azure Service Principal Secret"
   type        = string
-  default = "LGh8Q~pSrU~KEawcPRC2FN.8sgYrQg.OYx9rKdpN"
+  sensitive   = true
 }
 
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_SUBSCRIPTION_ID" {}
-variable "ARM_TENANT_ID" {}
+variable "ARM_CLIENT_ID" {
+  description = "Azure ARM Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure ARM Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
