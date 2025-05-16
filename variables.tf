dbcd1fd58d7a8d3ff0e21c2d437336d74be2436f
arana <arananth@hotmail.com>
2025-05-16 10:35:18 -0400

    fixing variable

--- variables.tf
+++ variables.tf
@@ -38,8 +38,3 @@ variable "ARM_SUBSCRIPTION_ID" {
   description = "Azure Subscription ID"
   type        = string
 }
-
-variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
-  type        = string
-}
